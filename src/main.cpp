d9cdfeb1456111170887a843672baf71652a6033
David Zevin <zevind25@gmail.com>
2024-02-07 13:57:31 -0700

    gave cars transforms

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ int main() {
     //creating the player car entity
     playerCar.name = "playerCar";
     playerCar.physType = PhysicsType::CAR;
+    playerCar.transform = new Transform();
     playerCar.car = new Car(playerCar.name.c_str(), PxVec3(0.0f, 0.0f, 0.0f), carRotateQuat, physicsSys.getPhysics(), physicsSys.getScene(), physicsSys.getGravity(), physicsSys.getMaterial());
 
     //adds the car to the carlist and the entity list
@@ -37,6 +38,7 @@ int main() {
     //Entity car2;
     //car2.name = "car2";
     //car2.physType = PhysicsType::CAR;
+    //car2.transform = new Transform();
     //car2.car = new Car(playerCar.name.c_str(), PxVec3(10.0f, 0.0f, -10.0f), PxQuat(PxIdentity), physicsSys.getPhysics(), physicsSys.getScene(), physicsSys.getGravity(), physicsSys.getMaterial());
 
     //adding the second car to the entity list
