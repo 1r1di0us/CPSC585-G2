5f952acc7cd32d9170413a4419347b735e47c990
BonnieWohoo <bonniewfwu@gmail.com>
2024-02-07 23:05:40 -0700

    Added in tinyobjloader
    Added tinyobjloader, not functional though

---
+++ src/Model.cpp
@@ -0,0 +1,14 @@
+#include "Model.h"
+
+void Model::loadModel(const std::string &filepath)
+{
+	// vertex attributes
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string warn, err;
+
+	if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+		throw std::runtime_error(warn + err);
+	}
+}
--- src/Model.h
+++ src/Model.h
@@ -2,14 +2,21 @@
 
 #include <glm/glm.hpp>
 #include <vector>
+#include <tinyobjloader/tiny_obj_loader.h>
+#include <stdexcept>
 
 struct Vertex {
-	glm::vec3 pos;
-	glm::vec3 col;
+	glm::vec3 position;
+	glm::vec3 color;
+	glm::vec3 normal;
+	glm::vec2 uv;
 };
 
 class Model {
 public:
 	std::vector<Vertex> verts;
 	glm::mat4 modelMatrix;
+
+	void loadModel(const std::string& filepath);
 };
+
