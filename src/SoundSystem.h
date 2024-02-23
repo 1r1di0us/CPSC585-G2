a7da01bbde9f1d78b53067d70064c839a80f657a
1r1di0us <57327873+1r1di0us@users.noreply.github.com>
2024-02-23 16:53:05 -0700

    added it as a library

---
+++ src/SoundSystem.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _AUDIO_ENGINE_H_
+#define _AUDIO_ENGINE_H_
+
+#include "fmod_studio.hpp"
+#include "fmod.hpp"
+#include <string>
+#include <map>
+#include <vector>
+#include <math.h>
+#include <iostream>
+
+#endif
