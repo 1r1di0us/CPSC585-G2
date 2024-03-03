fca9555614d179186bed763f93e23fac45f41e82
David Zevin <zevind25@gmail.com>
2024-03-03 03:46:43 -0700

    car respawn implemented
    big ass ting
    
    very cool algorithm seems fast enough
    
    tweaks to everything
    
    BIG (BUG FREE)

--- src/SharedDataSystem.h
+++ src/SharedDataSystem.h
@@ -9,6 +9,7 @@
 #include "Entity.h"
 #include <unordered_map>
 #include <memory>
+#include <chrono>
 
 using namespace physx;
 using namespace physx::vehicle2;
@@ -32,6 +33,13 @@ struct MapSquare {
 	std::vector<PxVec2> pointsInIt;
 	PxVec2 bottomLeft;
 	PxVec2 topRight;
+};
+
+// Comparator for priority queue based on distance
+struct CompareDistance {
+	bool operator()(const std::pair<float, PxVec2>& p1, const std::pair<float, PxVec2>& p2) {
+		return p1.first < p2.first;
+	}
 };
 
 class SharedDataSystem {
@@ -67,17 +75,49 @@ private:
 				const physx::PxU32 count) {}
 	};
 
+	/*
+	* CAR RESPAWN FUNCTIONS
+	*/
+
+	//gets rid of the y
+	PxVec2 ConvertVec3ToVec2(PxVec3 vectorToConvert);
+
+	//determines if a given point is in a square
+	bool IsPointInSquare(PxVec2 point, MapSquare& square);
+
+	//checks if the point is in the world bounds
+	bool IsPointInBounds(PxVec2 point);
+
+	// Function to calculate the distance between two PhysX vectors
+	float DistanceBetweenPoints(const PxVec2& point1, const PxVec2& point2);
+
+	//function to get the x nearest points given a list of points
+	std::vector<PxVec2> GetXNearestPoints(std::vector<PxVec2> pointList, int numPointsToGet, std::vector<PxVec2> pointsOfSameType);
+
+	//finds the center of four points
+	PxVec2 FindCenterOfFourPoints(std::vector<PxVec2> pointsList);
+
+	//populates the map square list with the points of the same type
+	void PopulateMapSquareList(std::vector<PxVec2> pointsOfSameType, std::vector<MapSquare>& mapSquareList);
+
+	//randomizes the map square list
+	void RandomizeMapSquareList(std::vector<MapSquare>& mapSquareList);
+
+	//generates a point a min distance away from all points in given vec and within the map range
+	PxVec3 GenerateSpawnPoint(std::vector<PxVec2> pointsOfSameType, PxReal minDistance, PxReal spawnHeight);
+
+
 public:
 
 	/*
 	* CONSTANTS:
 	*/
 
-	//respawn timer
-	const float RESPAWNLENGTH = 2.5f;
-
 	//timestep value, easily modifiable
 	const PxReal TIMESTEP = 1.0f / 60.0f;
+
+	//respawn timer
+	const float RESPAWNLENGTH = 2.5f;
 
 	//map coords for the corners
 	const PxVec2 BOTTOMLEFTMAPCOORD = PxVec2(-20.0f, -20.0f);
@@ -88,12 +128,12 @@ public:
 	const PxReal MAPLENGTHZ = TOPRIGHTMAPCOORD.y - BOTTOMLEFTMAPCOORD.y;
 
 	//the min distance cars can spawn from other cars
-	const PxReal CARMINSPAWNDISTANCE = 5.0f;
+	const PxReal CARMINSPAWNDISTANCE = 10.0f;
 
 	//the min spawn distance between powerups
 	const PxReal POWERUPSPAWNMINDISTANCE = 2.0f;
 
-	//the spawn height of cars (looks cool, but careful of flipping)
+	//the spawn height of cars
 	const PxReal CARSPAWNHEIGHT = 0.5f;
 
 	//the spawn height of powerups
@@ -104,6 +144,7 @@ public:
 	//the GOAT list of entities
 	std::vector<Entity> entityList;
 
+	//custom collision instance
 	ContactReportCallback* gContactReportCallback = new ContactReportCallback();
 
 	//PhysX management class instances.
@@ -132,23 +173,6 @@ public:
 
 	//gets the list of dead cars to do shit to
 	std::vector<CarInfo*> GetListOfDeadCars();
-
-	//gets rid of the y
-	PxVec2 ConvertVec3ToVec2(PxVec3 vectorToConvert);
-
-	//determines if a given point is in a square
-	bool IsPointInSquare(PxVec2 point, MapSquare& square);
-
-	//function to get the x nearest points given a list of points
-	std::vector<PxVec2> GetXNearestPoints(std::vector<PxVec2> pointList, int numPointsToGet, std::vector<PxVec2> pointsOfSameType);
-
-	//finds the center of four points
-	PxVec2 FindCenterOfFourPoints(std::vector<PxVec2> pointsList);
-
-	void PopulateMapSquareList(std::vector<PxVec2> pointsOfSameType, std::vector<MapSquare>& mapSquareList);
-
-	//generates a point a min distance away from all points in given vec and within the map range
-	PxVec3 GenerateSpawnPoint(std::vector<PxVec2> pointsOfSameType, PxReal minDistance, PxReal spawnHeight);
 
 	//returns a location where an entity can be respawned
 	PxVec3 DetermineSpawnLocation(PhysicsType physType);
