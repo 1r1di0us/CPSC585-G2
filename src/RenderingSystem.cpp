2d7d2407237fc4088a3922d08474ce361754dd91
Zainalabadeen Jelani <zainjelani21@gmail.com>
2024-03-04 01:15:20 -0700

    Add a toggleable boolean for bird's eye camera view for debugging purposes.

--- src/RenderingSystem.cpp
+++ src/RenderingSystem.cpp
@@ -132,22 +132,23 @@ void RenderingSystem::updateRenderer(std::shared_ptr<std::vector<Entity>> entity
     glm::quat playerRot = entityList->at(0).transform->getRot();
     //std::cout << playerPos.x << ":" << playerPos.y << ":" << playerPos.z << std::endl;
 
-    // Calculate the point the camera should look at (e.g., slightly above the player)
-    glm::vec3 offsetFromPlayer = glm::vec3(0.0f, 8.0f, 20.0f);
-    camera.Position = playerPos + offsetFromPlayer;
-    glm::vec3 lookAtPoint = playerPos + glm::vec3(0.0f, 1.0f, 0.0f);
 
-    //// Camera things
-    view = glm::lookAt(camera.Position, lookAtPoint, camera.Up);
+    if (useBirdsEyeView) {
+        // Bird's eye view
+        glm::vec3 cameraPosition = glm::vec3(0.0f, 50.0f, 0.0f);
+        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+        glm::vec3 cameraUp = glm::vec3(0.0f, 0.0f, -1.0f);
 
-    //bird's eye view
-    //Define camera parameters
-    //glm::vec3 cameraPosition = glm::vec3(0.0f, 50.0f, 0.0f); // Position the camera above the scene
-    //glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f); // Look at the center of the scene
-    //glm::vec3 cameraUp = glm::vec3(0.0f, 0.0f, -1.0f); // Define the up vector
+        view = glm::lookAt(cameraPosition, cameraTarget, cameraUp);
+    }
+    else {
+        // Original view
+        glm::vec3 offsetFromPlayer = glm::vec3(0.0f, 8.0f, 20.0f);
+        camera.Position = playerPos + offsetFromPlayer;
+        glm::vec3 lookAtPoint = playerPos + glm::vec3(0.0f, 1.0f, 0.0f);
 
-    //// Calculate the view matrix using glm::lookAt
-    //view = glm::lookAt(cameraPosition, cameraTarget, cameraUp);
+        view = glm::lookAt(camera.Position, lookAtPoint, camera.Up);
+    }
 
     // car translating
     model = glm::translate(model, playerPos);
