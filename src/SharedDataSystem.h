a91232c68907eb24b0390c7ee47e5dc4b5bc1607
David Zevin <zevind25@gmail.com>
2024-03-04 23:10:27 -0700

    Update SharedDataSystem.h
    tweaked spawn rates of powerups

--- src/SharedDataSystem.h
+++ src/SharedDataSystem.h
@@ -177,10 +177,10 @@ public:
 	const PxReal POWERUP_SPAWN_HEIGHT = 0.0f;
 
 	//the spawn rate of a random powerup
-	const float RANDOM_POWERUP_SPAWN_RATE = 5.0f;
+	const float RANDOM_POWERUP_SPAWN_RATE = 50.0f;
 
 	//the spawn rate of an ammo spawn powerup
-	const float AMMO_POWERUP_SPAWN_RATE = 3.0f;
+	const float AMMO_POWERUP_SPAWN_RATE = 10.0f;
 
 	//the max number of ammo spawn powerups
 	const int NUMBER_OF_AMMO_POWERUPS = 3;
