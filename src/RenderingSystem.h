964753f99f6ab814ea9f7e45f2eab51d4c891b37
Zainalabadeen Jelani <zainjelani21@gmail.com>
2024-04-03 18:39:11 -0600

    Add respawn textures.

--- src/RenderingSystem.h
+++ src/RenderingSystem.h
@@ -106,7 +106,7 @@ public:
 	Shader particleShader;
 	void initParticlesVAO();
 
-	unsigned int player1Texture, player2Texture, player3Texture, player4Texture, player5Texture, redTexture,
+	unsigned int player1Texture, player2Texture, player3Texture, player4Texture, player5Texture, playerInvincibleTexture, redTexture,
 		menuPlay, menuControls, menuQuit, controlsMenu, pauseMenuContinue, pauseMenuQuit,
 		resultsP1, resultsP2, resultsP3, resultsP4, resultsP5, resultsTie,
 		planeTexture, gunMetalTexture, parryTexture, ammoPowerupTexture, projectileSpeedPowerupTexture, projectileSizePowerupTexture, armourPowerupTexture, particleExplosionTexture, particleSmokeTexture;
