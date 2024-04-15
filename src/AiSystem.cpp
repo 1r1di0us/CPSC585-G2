2d77483d1fb3a0dd9a0d43c4c468b2f11690bd18
1r1di0us <57327873+1r1di0us@users.noreply.github.com>
2024-04-14 20:46:24 -0600

    fixed ai super good
    don't worry guys they aren't super garbage

--- src/AiSystem.cpp
+++ src/AiSystem.cpp
@@ -12,7 +12,7 @@ AiSystem::AiSystem(SharedDataSystem* dataSys, EngineDriveVehicle* aiCar) {
 	coolDownTimer = 0;
 	reverseTimer = 0;
 	lockOnTime = 0;
-	navMesh = new NavMesh(dataSys		);
+	navMesh = new NavMesh(dataSys);
 	pathFinder = new PathFinder(navMesh);
 	moveLocation = PxVec3(-100, -100, -100);
 	moveNode = nullptr;
@@ -27,13 +27,13 @@ AiSystem::AiSystem(SharedDataSystem* dataSys, EngineDriveVehicle* aiCar) {
 	centerNodes.emplace_back(navMesh->nodes->at(469)); //[15][19] nice
 
 	edgeNodes.emplace_back(navMesh->nodes->at(62)); //[2][2]
-	edgeNodes.emplace_back(navMesh->nodes->at(165)); //[5][15]
+	edgeNodes.emplace_back(navMesh->nodes->at(105)); //[3][15]
 	edgeNodes.emplace_back(navMesh->nodes->at(87)); //[2][27]
-	edgeNodes.emplace_back(navMesh->nodes->at(444)); //[14][24]
+	edgeNodes.emplace_back(navMesh->nodes->at(446)); //[14][26]
 	edgeNodes.emplace_back(navMesh->nodes->at(837)); //[27][27]
-	edgeNodes.emplace_back(navMesh->nodes->at(734)); //[24][14]
+	edgeNodes.emplace_back(navMesh->nodes->at(794)); //[26][14]
 	edgeNodes.emplace_back(navMesh->nodes->at(812)); //[27][2]
-	edgeNodes.emplace_back(navMesh->nodes->at(455)); //[15][5]
+	edgeNodes.emplace_back(navMesh->nodes->at(453)); //[15][3]
 }
 
 bool AiSystem::update(std::chrono::duration<double> deltaTime) {
@@ -422,7 +422,9 @@ bool AiSystem::hiding_behaviour(bool fire, std::chrono::duration<double> deltaTi
 				int prevNode;
 				if (nodeIterator + 1 == (int)edgeNodes.size()) nextNode = 0;
 				else nextNode = nodeIterator + 1;
-				if (nodeIterator == 0) prevNode = (int)edgeNodes.size() - 1;
+				if (nodeIterator == 0) {
+					prevNode = (int)edgeNodes.size() - 1;
+				}
 				else prevNode = nodeIterator - 1;
 				if ((edgeNodes[nextNode]->centroid - (carPos + aiCar->mPhysXState.physxActor.rigidBody->getGlobalPose().q.getBasisVector2())).magnitude() <=
 					(edgeNodes[prevNode]->centroid - (carPos + aiCar->mPhysXState.physxActor.rigidBody->getGlobalPose().q.getBasisVector2())).magnitude()) {
